af45eb8a340dbd9301cb25b6104628b0a8e9d741
Chaitanya Deshmukh <cd20082000@gmail.com>
2024-12-31 19:47:35 +0530

    Update part1.py

--- part1.py
+++ part1.py
@@ -5,23 +5,23 @@ import os
 
 app = Flask(__name__)
 
-# Use AWS CLI credentials automatically (credentials stored via 'aws configure')
+
 s3 = boto3.client(
     's3',
-    region_name='ap-southeast-1'  # Replace with your AWS region if needed
+    region_name='ap-southeast-1'  
 )
 
-# Define the name of your S3 bucket
+
 BUCKET_NAME = 'part1httpservice'
 
-# Function to list content in S3 for the given path
+
 def list_s3_content(path=''):
     try:
-        # If path is provided, list the objects within that path
+       
         if path:
             response = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=path, Delimiter='/')
         else:
-            # If no path is specified, list the top-level contents
+           
             response = s3.list_objects_v2(Bucket=BUCKET_NAME, Delimiter='/')
 
         contents = []
